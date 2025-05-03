4f3fd40706ea293dbf8729fdb836e3817e3cb9d3
GameParrot <85067619+GameParrot@users.noreply.github.com>
2025-05-03 15:19:42 -0400

    add more fmod options, make fmod use aaudio

---
+++ include/mcpelauncher/fmod_utils.h
@@ -0,0 +1,18 @@
+#pragma once
+
+class FmodUtils {
+private:
+    static int (*FMOD_System_Init)(void* system, int maxchannels, unsigned int flags, void* extradriverdata);
+    static int (*FMOD_System_SetSoftwareFormat)(void* system, int samplerate, int speakermode, int numrawspeakers);
+    static int (*FMOD_System_SetDSPBufferSize)(void* system, unsigned int bufferlength, int numbuffers);
+    static int (*FMOD_System_GetDSPBufferSize)(void* system, unsigned int* bufferlength, int* numbuffers);
+
+    static int sampleRate;
+
+public:
+    static void setup(void* handle);
+    static int initHook(void* system, int maxchannels, unsigned int flags, void* extradriverdata);
+    static void setSampleRate(int newSampleRate) {
+        sampleRate = newSampleRate;
+    };
+};
